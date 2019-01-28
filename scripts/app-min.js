a273da0eac52fbc4db60fc8690c200672b573f3d
J Law <justin.eric.law@gmail.com>
2019-01-28 12:48:32 -0800

    Mobile menu js, styles, structure.

---
+++ scripts/app-min.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.querySelector(".hamburger-toggle").addEventListener("click",function(){document.querySelector(".header-container").classList.toggle("nav-mobile-menu-open")});var e=new ScrollTrigger});
--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,12 @@
 // Scroll Trigger
 document.addEventListener('DOMContentLoaded', function(){
+
+  // Mobile/Tablet Menu
+  document.querySelector('.hamburger-toggle').addEventListener('click', function () {
+    document.querySelector('.header-container').classList.toggle('nav-mobile-menu-open');
+  });
+
+  // ScrollTrigger Animation
   var trigger = new ScrollTrigger();
+
 });
